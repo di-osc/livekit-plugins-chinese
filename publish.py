d9754b580b29f4e17f722a89b3a0d5aac4d78656
wangmengdi <790990241@qq.com>
2025-06-15 07:36:33 +0800

    fix publish

--- publish.py
+++ publish.py
@@ -2,19 +2,30 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
+from jsonargparse import auto_cli
 
 load_dotenv()
 
-plugins = Path("livekit-plugins")
-for plugin in plugins.iterdir():
-    if not plugin.is_dir():
-        continue
-    if not (plugin / "pyproject.toml").exists():
-        continue
-    print(f"Building {plugin.name}")
-    os.system(f"cd {plugin} && uv build")
 
-pypi_token = os.getenv("PYPI_TOKEN")
+def run(version: str):
+    plugins = Path("livekit-plugins")
+    for plugin in plugins.iterdir():
+        if not plugin.is_dir():
+            continue
+        if not (plugin / "pyproject.toml").exists():
+            continue
+        plugin_name = plugin.stem.replace("livekit-plugins-", "")
+        print(f"Building {plugin_name} for version {version}")
+        # 修改version.py文件
+        with open(plugin / f"livekit/plugins/{plugin_name}" / "version.py", "w") as f:
+            f.write(f'''__version__ = "{version}"''')
+        print(f"Building {plugin.name} for version {version}")
+        os.system(f"cd {plugin} && uv build")
 
-if pypi_token:
-    os.system(f"uv publish --token {pypi_token}")
+    pypi_token = os.getenv("PYPI_TOKEN")
+    if pypi_token:
+        os.system(f"uv publish --token {pypi_token}")
+
+
+if __name__ == "__main__":
+    auto_cli(run)
