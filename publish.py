b94e50438e631ff4cf92e82bfed145d7f4963500
wangmengdi <790990241@qq.com>
2025-08-18 10:52:31 +0800

    固定livekit-agents版本为1.2.2，并更新相关构建逻辑

--- publish.py
+++ publish.py
@@ -22,8 +22,8 @@ def run(version: str, publish: bool = True):
         plugin_name = plugin.stem.replace("livekit-plugins-", "")
         print(f"Building {plugin_name} for version {version}")
         # 修改livekit-agent版本
-        agent_version = version.split(".post")[0]
-        os.system(f'''cd {plugin} && uv add "livekit-agents>={agent_version}"''')
+        # agent_version = version.split(".post")[0]
+        os.system(f'''cd {plugin} && uv add "livekit-agents==1.2.2"''')
         # 修改version.py文件
         with open(plugin / f"livekit/plugins/{plugin_name}" / "version.py", "w") as f:
             f.write(f'''__version__ = "{version}"''')
